7b9be6a5c82146270d347dfed742d606eecdf735
Gracerue <munyaradzi045@gmail.com>
2023-05-23 20:17:11 +0200

    Fix linters

--- person.rb
+++ person.rb
@@ -30,10 +30,3 @@ class Person < Nameable
     of_age? || @parent_permission
   end
 end
-
-person = Person.new(22, 'maximilianus')
-puts person.correct_name
-capitalizedPerson = CapitalizeDecorator.new(person)
-puts capitalizedPerson.correct_name
-capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
-puts capitalizedTrimmedPerson.correct_name
