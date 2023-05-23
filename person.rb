43a09bb623a79d38eb0f7e71ffd6263cd81079e1
Gracerue <munyaradzi045@gmail.com>
2023-05-23 19:49:01 +0200

    Add decorators

--- person.rb
+++ person.rb
@@ -1,9 +1,18 @@
-class Person
+require_relative 'nameable'
+require_relative 'capitalize_decorator'
+require_relative 'trimmer_decorator'
+
+class Person < Nameable
   def initialize(age, name = 'unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   attr_reader :id
@@ -21,3 +30,10 @@ class Person
     of_age? || @parent_permission
   end
 end
+
+person = Person.new(22, 'maximilianus')
+puts person.correct_name
+capitalizedPerson = CapitalizeDecorator.new(person)
+puts capitalizedPerson.correct_name
+capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+puts capitalizedTrimmedPerson.correct_name
