b645add6ff952b17d0b337e5135b53de53593b61
Gracerue <munyaradzi045@gmail.com>
2023-05-24 13:22:12 +0200

    Create class Classroom

--- person.rb
+++ person.rb
@@ -1,6 +1,10 @@
 require_relative 'nameable'
 require_relative 'capitalize_decorator'
 require_relative 'trimmer_decorator'
+require_relative 'book'
+require_relative 'Rental'
+require 'date'
+
 
 class Person < Nameable
   def initialize(age, name = 'unknown', parent_permission: true)
@@ -9,14 +13,19 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def correct_name
     @name
   end
 
-  attr_reader :id
+  attr_reader :id, :rentals
   attr_accessor :name, :age, :parent_permission
+
+  def add_rental(book, date)
+    Rental.new(date, book, self)
+  end
 
   private
 
@@ -30,3 +39,12 @@ class Person < Nameable
     of_age? || @parent_permission
   end
 end
+
+person1 = Person.new(25, 'John Doe')
+date1 = Date.today
+book1 = Book.new('Title 1', 'Author 1')
+rental1 = book1.add_rental(person1, date1)
+puts rental1.person == person1 # Expected: true
+puts rental1.book == book1 # Expected: true
+puts person1.rentals.include?(rental1) # Expected: true
+puts book1.rentals.include?(rental1) # Expected: true
--- student.rb
+++ student.rb
@@ -5,7 +5,15 @@ class Student < Person
 
   def initialize(age, name = 'unknown', parent_permission: true, classroom: nil)
     super(age, name, parent_permission: parent_permission)
+    self.classroom = classroom
+  end
+
+  def classroom=(classroom)
+    if @classroom
+      @classroom.students.delete(self)
+    end
     @classroom = classroom
+    classroom.students << self if classroom
   end
 
   def play_hooky
