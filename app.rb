7bd918f55c7f2e522686cbc22ac7c5dd5c789b8f
Gracerue <munyaradzi045@gmail.com>
2023-05-25 17:13:14 +0200

    Fix linter errors

--- app.rb
+++ app.rb
@@ -16,9 +16,9 @@ class App
 
   def list_books
     if @books.empty?
-      puts "There are no books"
+      puts 'There are no books'
     else
-      puts "List of books:"
+      puts 'List of books:'
       @books.each do |book|
         puts "Title: #{book.title}, Author: #{book.author}"
       end
@@ -27,48 +27,56 @@ class App
 
   def list_people
     if @people.empty?
-      puts "There are no people"
+      puts 'There are no people'
     else
-      puts "List of people:"
+      puts 'List of people:'
       @people.each do |person|
         puts "ID: #{person.id}, Name: #{person.correct_name}, Age: #{person.age}"
       end
     end
   end
 
+  def create_student
+    print 'Age: '
+    age = gets.chomp.to_i
+    print 'Name: '
+    name = gets.chomp
+    print 'Does the student have permission? [true/false]: '
+    parent_permission = gets.chomp.downcase == 'true'
+    print "Enter student's classroom (optional): "
+    classroom = gets.chomp
+    class_name = Classroom.new(classroom)
+    student = Student.new(age, name, parent_permission: parent_permission, classroom: class_name)
+    @people << student
+  end
+
+  def create_teacher
+    print 'Age: '
+    age = gets.chomp.to_i
+    print 'Name: '
+    name = gets.chomp
+    print 'Does the teacher have permission? [true/false]: '
+    parent_permission = gets.chomp.downcase == 'true'
+    print 'Enter teacher\'s specialization (optional): '
+    specialization = gets.chomp
+
+    teacher = Teacher.new(age, name, parent_permission: parent_permission, specialization: specialization)
+    @people << teacher
+  end
+  
   def create_person
     puts 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
     type = gets.chomp.to_i
 
     case type
     when 1
-      print 'Age: '
-      age = gets.chomp.to_i
-      print 'Name: '
-      name = gets.chomp
-      print 'Does the student have permission? [true/false]: '
-      parent_permission = gets.chomp.downcase == 'true'
-      print "Enter student's classroom (optional): "
-      classroom = gets.chomp
-      class_name = Classroom.new(classroom)
-      student = Student.new(age, name, parent_permission: parent_permission, classroom: class_name)
-      @people << student
-      puts "Student has been successfully created"
+      create_student
+      puts 'Student has been successfully created'
     when 2
-      print 'Age: '
-      age = gets.chomp.to_i
-      print 'Name: '
-      name = gets.chomp
-      print 'Does the teacher have permission? [true/false]: '
-      parent_permission = gets.chomp.downcase == 'true'
-      print "Enter teacher's specialization (optional): "
-      specialization = gets.chomp
-
-      teacher = Teacher.new(age, name, parent_permission: parent_permission, specialization: specialization)
-      @people << teacher
-      puts "Teacher has been created successfully"
+      create_teacher
+      puts 'Teacher has been created successfully'
     else
-      puts "Invalid person type, please choose either student (1) or teacher (2)."
+      puts 'Invalid person type, please choose either student (1) or teacher (2).'
     end
   end
 
@@ -80,35 +88,35 @@ class App
 
     new_book = Book.new(title, author)
     @books << new_book
-    puts "Book #{new_book.title} has been successfully created"
+    puts 'Book #{new_book.title} has been successfully created'
   end
 
   def create_rental
     if @people.empty?
-      puts "Person not found"
+      puts 'Person not found'
     elsif @books.empty?
-      puts "Book not found"
+      puts 'Book not found'
     else
-      puts "Select a book from the following list by number"
+      puts 'Select a book from the following list by number'
       @books.each_with_index do |book, index|
         puts "#{index})Title: #{book.title} Author: #{book.author}"
       end
 
-      print("Book number: ")
+      print('Book number: ')
       book_choice = gets.chomp.to_i
 
-      puts "Select person by number not ID from the list below"
+      puts 'Select person by number not ID from the list below'
       @people.each_with_index do |person, index|
         puts "#{index}) Name: #{person.correct_name} Id: #{person.id} Age: #{person.age}"
       end
 
-      print("Person number: ")
+      print('Person number: ')
       person_number = gets.chomp.to_i
-      print("Date: ")
+      print('Date: ')
       date = gets.chomp.to_s
       rental = Rental.new(date, @books[book_choice], @people[person_number]) 
       @rentals << rental
-      puts "Rental created successfully"
+      puts 'Rental created successfully'
     end
   end
 
@@ -116,7 +124,7 @@ class App
     person = @people.find { |p| p.id == person_id }
 
     if person.nil?
-      puts "Person not found"
+      puts 'Person not found'
     else
       puts "Rentals for person: #{person.correct_name}"
       rentals = @rentals.select { |r| r.person == person }
--- main.rb
+++ main.rb
@@ -6,15 +6,15 @@ class Main
   end
 
   def list_options
-    puts "Welcome, choose an option below:\n"
+    puts 'Welcome, choose an option below:\n'
     actions = [
-      "1 - List all books",
-      "2 - List all people",
-      "3 - Create a person",
-      "4 - Create a book",
-      "5 - Create a rental",
-      "6 - List all rentals for a given person id",
-      "7 - Exit"
+      '1 - List all books',
+      '2 - List all people',
+      '3 - Create a person',
+      '4 - Create a book',
+      '5 - Create a rental',
+      '6 - List all rentals for a given person id',
+      '7 - Exit'
     ]
     puts actions
   end
@@ -28,7 +28,7 @@ class Main
   def main
     loop do
       list_options
-      choice = get_request("Enter your choice: ", :to_i)
+      choice = get_request('Enter your choice: ', :to_i)
 
       case choice
       when 1
@@ -42,12 +42,12 @@ class Main
       when 5 
         @app.create_rental
       when 6
-        person_id = get_request("Enter person ID: ", :to_i)
+        person_id = get_request('Enter person ID: ', :to_i)
         @app.list_rentals(person_id)
       when 7
         break
       else
-        puts "Invalid choice, please try again"
+        puts 'Invalid choice, please try again'
       end
     end
   end
